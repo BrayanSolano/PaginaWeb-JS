6944ce7046b581baae7f5a354e312f71b23ba31c
BrayanSolano <82620231+BrayanSolano@users.noreply.github.com>
2023-03-16 20:23:52 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+})
+
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase', {delay: 800});
+ScrollReveal().reveal('.new-cards', {delay: 800});
+ScrollReveal().reveal('.card-banner-one', {delay: 800});
+ScrollReveal().reveal('.card-banner-two', {delay: 800});
+
+
